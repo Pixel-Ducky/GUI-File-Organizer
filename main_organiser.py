7ad97a80db314a254d5cd13d670ceca8447c3a00
pixel Duck <skytosprout@gmail.com>
2025-04-13 14:52:48 +0300

    refactored prototype & fixed issue when Folder=None
    refactored the entire file so it will be easier to implement pyqt6 later on .
    In the previous commit if a Folder to be organized isnt found , it crashes.
    But  now if a Folder isnt found it says "Couldn't find Folder"

--- main_organiser.py
+++ main_organiser.py
@@ -11,33 +11,12 @@ path_to_json_file = Path("file_extensions.json")
 
 
 #search for a Folder on my system , mixed_files will be replaced by pyqt6 folder search
-for path in home_path.rglob("mixed_files") :
-    if path.is_dir():
-        found_path = path
-        #print(found_path)
-        break
-
-# extensions ={
-#     ".jpg" :"Pictures\\pics",
-#     ".png" :"Pictures\\pics",
-#     ".jpeg" :"Pictures\\pics",
-#     ".gif" :"Pictures\\pics",
-#     ".jfif" :"Pictures\\pics",
-#     ".pdf" :"Desktop\\Books",
-#     ".epub" :"Desktop\\Books",
-#     ".docx" :"Documents\\Folder_Projects",
-#     ".pptx" :"Documents\\Folder_Projects",
-#     ".xls" :"Documents\\Folder_Projects",
-#     ".txt" :"Documents\\Folder_Notepad",
-#     ".mp4" :"Documents\\Folder_Video",
-#     ".mp3" :"Documents\\Folder_Audio",
-#     ".wav" :"Documents\\Folder_Audio",
-# }
-
-
-#file_name.txt
-
-
+def search_for_folder():#if it dosent find a folder it will return None by default , no need to type it.
+    for path in home_path.rglob("mixed_files") :
+        if path.is_dir():
+            found_path = path
+            return found_path
+        
 
 
 def read_from_json_file():
@@ -49,33 +28,48 @@ def read_from_json_file():
         return None
     
 
-extension_table = read_from_json_file()
 
-for file in os.listdir(found_path): #list contents of Folder
-    filepath_for_file_in_Folder = Path(os.path.join(found_path, file) ) #construct absolute filepath for all contents
+def select_file_extension(path):
+    special_tuple = os.path.splitext(str(path)) #split path & filename from text , returns tuple(name , extension)
+    file_extension = special_tuple[1].lower().strip() #select only the extension
+    return file_extension
 
-    if filepath_for_file_in_Folder.is_file(): # check if content is a file
-        special_tuple = os.path.splitext(str(filepath_for_file_in_Folder)) #split path & filename from text , returns tuple(name , extension)
-        file_extension = special_tuple[1].lower().strip() #select only the extension
-
-        if file_extension in extension_table:
+def check_if_extension_in_json(file_extension, extension_table):
+    
+    if file_extension in extension_table:
             folder_where_file_should_be_stored = extension_table[file_extension]
             final_absolute_path =  Path(os.path.join(home_path, folder_where_file_should_be_stored) )
+            return final_absolute_path
+    else:
+        return None
+    
 
-            try:
-                if final_absolute_path.exists():
-                    shutil.move(filepath_for_file_in_Folder ,final_absolute_path )
-                    
-                else:
-                    os.makedirs(final_absolute_path, exist_ok=True)
-                    print('creating directories')
-            except shutil.Error  :
-                    print(f'this file "{file}" is already in the destination folder.\n change the name manually , we wont move it.\n')
-                
-        
-           
-        else: 
-            print(f'{file_extension} is not inside json filetable')
+
+found_path = search_for_folder()
+extension_table = read_from_json_file()
+
+if found_path is None :
+    print(f'sorry , we havent found any Folder with that name')
+
+else:
+    for file in os.listdir(found_path): #list contents of Folder
+        filepath_for_file_in_Folder = Path(os.path.join(found_path, file) ) #construct absolute filepath for all contents
+
+        if filepath_for_file_in_Folder.is_file(): # check if content is a file
+            file_extension = select_file_extension(filepath_for_file_in_Folder)
+            checked = check_if_extension_in_json(file_extension , extension_table)
+
+            if checked:                
+                try:
+                    if checked.exists():
+                        shutil.move(filepath_for_file_in_Folder ,checked )    
+                    else:
+                        os.makedirs(checked, exist_ok=True)
+                        print('creating directories')
+                except shutil.Error  :
+                        print(f'this file "{file}" is already in the destination folder.\n change the name manually , we wont move it.\n')              
+            else: 
+                print(f'{file_extension} is not inside json filetable')
 
 
 
